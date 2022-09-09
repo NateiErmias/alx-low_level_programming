964c816391ac728ed7cb14d349c7651f36f8d0c1
NateiErmias <natei.ermias@aau.edu.et>
2022-09-09 06:04:43 -0700

    0 task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,45 +8,23 @@
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
-
 int main(void)
-
 {
-
-	int n;
-
-
-
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 /*Code goes below here */
-
-	if (n > 0)
-
-	{
-
-			printf("%i is positive\n", n);
-
-	}
-
-	else if (n == 0)
-
-	{
-
-			printf("%i is zero\n", n);
-
-	}
-
-	else if (n < 0)
-
-	{
-
-			printf("%i is negative\n", n);
-
-	}
-
-	return (0);
-
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+return (0);
 }
