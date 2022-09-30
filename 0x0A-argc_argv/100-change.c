3613beb879dd09fa60c0ed15a6ef22040b5a02b8
NateiErmias <natei.ermias@aau.edu.et>
2022-09-30 15:23:58 -0700

    0x0A

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,6 @@
 *        make change for an amount of money.
 * @argc: The number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments.
-*
 * Return: If the number of arguments is not exactly one - 1.
 *         Otherwise - 0.
 */
@@ -25,19 +24,24 @@ if ((cents - 25) >= 0)
 {
 cents -= 25;
 continue;
+}
 if ((cents - 10) >= 0)
+{
 cents -= 10;
 continue;
-}			
+}
 if ((cents - 5) >= 0)
 {
 cents -= 5;
 continue;
 }
 if ((cents - 2) >= 0)
+{
 cents -= 2;
 continue;
+}
+cents--;
+}
 printf("%d\n", coins);
 return (0);
-}
 }
