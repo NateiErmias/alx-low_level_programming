024014bd6ff00890f4c0148e8accc82514370b24
NateiErmias <natei.ermias@aau.edu.et>
2022-10-10 11:45:32 -0700

    natei task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,15 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
+/*
+* File: dog.h
+* Auth: (cm-amos)Amos Mwongela
+* Desc: Header file that defines a new type struct dog.
+*/
 /**
-* struct dog - Dog attributes
+* struct dog - A new type describing a dog.
 * @name: The name of the dog.
 * @age: The age of the dog.
 * @owner: The owner of the dog.
-*
-* Description: The attributes of a dog.
 */
 struct dog
 {
@@ -15,7 +18,7 @@ float age;
 char *owner;
 };
 /**
-* dog_t - Typedef for dog structure
+* dog_t - Typedef for struct dog
 */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
