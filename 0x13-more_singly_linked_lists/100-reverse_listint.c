d1cac9bb4c6156560a45852313a5735c861d3489
NateiErmias <natei.ermias@aau.edu.et>
2022-10-24 12:24:17 -0700

    0x013c task

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,16 +1,9 @@
 #include "lists.h"
-
 /**
- *
- * * reverse_listint - reverses a linked list
- *
- * * @head: pointer to the first node in the list
- *
- * *
- *
- * * Return: pointer to the first node in the new list
- *
- * */
+* reverse_listint - reverses a linked list
+* @head: pointer to the first node in the list
+* Return: pointer to the first node in the new list
+*/
 
 listint_t *reverse_listint(listint_t **head)
 
